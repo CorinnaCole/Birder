9e2c360c55eeb68956f5c1d81a34841365cb57ed
Corinna Cole <corinna.j.cole@gmail.com>
2023-02-08 10:17:49 -0600

    Center modal, add header and MUI close icon

--- src/BirdsPage/BirdDetail.jsx
+++ src/BirdsPage/BirdDetail.jsx
@@ -1,86 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components';
 import Card from '@mui/material/Card';
-import ReactCardFlip from 'react-card-flip';
+import CloseIcon from '@mui/icons-material/Close';
 import Map from '../Map.jsx';
 import birdphotos from './planz.jsx';
+import {
+  BirdDetailDiv,
+  BirdDetailHeader,
+  BirdDetailBackground,
+  DescriptionDiv
+} from '../styled/StyledBirdComponents.jsx';
 
-const SectionCard = styled.aside`
-    border: 4mm ridge #213547;
-    // box-shadow: 5px 5px 5px #213547;;
-    padding: 2rem;
-    // background-color: #2d5d7b;
-    background:
-    linear-gradient(lightSkyBlue, transparent),
-    linear-gradient(to top left, #686868, transparent),
-    linear-gradient(to top right, #213547, transparent);
-    background-blend-mode: screen;
-  `;
 
-const SectionCard2 = styled.aside`
-    border: 4mm ridge #213547;
-    padding: 2rem;
-    background:
-    linear-gradient(lightSkyBlue, transparent),
-    linear-gradient(to top left, #686868, transparent),
-    linear-gradient(to top right, #213547, transparent);
-    background-blend-mode: screen;
-  `;
 
 const SectionImage = styled.img`
   margin-top: -1.25rem;
   margin-left: -1.25rem;
   width: calc(100% + 1.5rem);
   height: 214px;
-  object-fit: cover;
-  border: 6px solid #213547;
-  box-shadow: 5px 5px 5px #213547;
+  object-fit: contain;
 `;
 
-const Tag = styled.span`
 
-  padding: .5px;
-  text-align: center;
-  background: gray;
-  color: white;
-  font-size: 15px;
-  // padding: 4px 8px;
-  // border: 1px solid #e5eaed;
-  // border-radius: 50px;
-  // font-size: 12px;
-  // font-weight: 600;
-  // color: #83c9f4;
- `;
 const Decription = styled.p`
   font-size: 14px;
   color: #7f8c9b;
   line-height: 150%;
-  color: white;
-  // padding: 16px 8px 8px 8px;
+  color: black;
  `;
 
-const Container = styled.div`
-  box-shadow: 10px 10px 10px #213547;;
-  border-radius: 20px;
-  border: 10px solid #686868;
-  max-width: 400px;
-  margin: 0 auto;
-  margin-top: 20vh;
-  `;
-
-const Container2 = styled.div`
-  box-shadow: 10px 10px 10px #213547;;
-  border-radius: 20px;
-  border: 10px solid #686868;
-  // max-width: 400px;
-  margin: 0 auto;
-  margin-top: 20vh;
-  width: 40vh;
-  `;
-
-
 const BirdDetail = ({ bird, back }) => {
-  const [flip, setFlip] = useState(false);
 
   useEffect(() => {
     console.log(bird, 'here is the bird');
@@ -88,45 +37,45 @@ const BirdDetail = ({ bird, back }) => {
   }, [])
 
   return (
-    <div>
-        <Container>
-          <SectionCard onClick={() => setFlip(!flip)}>
-            <aside>
-              <h3>{bird.scentific_name}</h3>
-              <h3>AKA: {bird.common_name}</h3>
-              <SectionImage
-                src={birdphotos[bird.bird_id - 1] || bird.bird_photos[0]}
-                alt="header image"
-                height="400"
-                width="384"
-              />
-              <div>First Seen: {bird.first_seen}</div>
-              <div>Last Seen: {bird.last_seen}</div>
-              <div>Times seen: {bird.count}</div>
-              <h3>Decription</h3>
-              <Decription>
-                {bird.summary}
-              </Decription>
-              <h3>Personal Notes</h3>
-              <Decription>{bird.sighting_notes[0]}</Decription>
-            </aside>
-          </SectionCard>
-        </Container>
-        <Container2>
-          <SectionCard2>
-            <aside>
-              <h3 onClick={() => setFlip(!flip)}>Sighting Locations</h3>
-              <Map birdArrayLocations={bird.bird_location} />
-            </aside>
-          </SectionCard2>
+    <BirdDetailBackground>
+      <BirdDetailDiv>
+        <BirdDetailHeader>
+          {bird.common_name}: {bird.scentific_name}
+          <CloseIcon
+            onClick={() => {back()}}
+            sx={{
+              color: "white",
+              fontSize: 40,
+              cursor: "pointer"
+            }}
+          />
+        </BirdDetailHeader>
+        <DescriptionDiv>
+          <SectionImage
+            src={birdphotos[bird.bird_id - 1] || bird.bird_photos[0]}
+            alt="header image"
+            height="400"
+            width="384"
+          />
+          <aside>
 
-        </Container2>
-      <div>
-        <button onClick={() => {
-          back()
-        }} style={{ marginTop: 50 }}>back</button>
-      </div>
-    </div>
+            <h2>{bird.scentific_name}</h2>
+
+            <div>Times seen: {bird.count}</div>
+            <h3>Decription</h3>
+            <Decription>
+              {bird.summary}
+            </Decription>
+            <h3>Personal Notes</h3>
+            <Decription>{bird.sighting_notes[0]}</Decription>
+          </aside>
+          <aside>
+            <h3 >Sighting Locations</h3>
+            <Map birdArrayLocations={bird.bird_location} />
+          </aside>
+        </DescriptionDiv>
+      </BirdDetailDiv>
+    </BirdDetailBackground>
   );
 };
 export default BirdDetail;
--- src/styled/StyledBirdComponents.jsx
+++ src/styled/StyledBirdComponents.jsx
@@ -46,12 +46,66 @@ const ImgDiv = styled.div`
   border-radius: 9999px;
   background: url(${props => props.background});
   background-size: 100%;
-`
+`;
 
 const HeaderDiv = styled.div`
 height: auto;
 padding-left: 40px;
 line-height: 1em;
+`;
+
+const BirdDetailBackground = styled.div`{
+  width: 100%;
+  height: 100%;
+  background-color: #D3D3D3;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}`;
+
+const BirdDetailDiv = styled.div`
+  width: 90vw;
+  max-width: 1577px;
+  height: 95vh;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: 3px solid red;
+  border-radius: 10px;
+  background-color: white;
+  display: grid;
+  grid-template-columns: 5% 2fr 2fr 1fr 2fr 2fr 5%;
+  grid-template-rows: 5% repeat(8, 1fr) 5%;
+  `;
+
+  const BirdDetailHeader = styled.div`
+  grid-area: 1/ 1 / 1 / 8;
+  background-color: #32690f;
+  color: white;
+  border-radius: 10px 10px 0px 0px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1% 0 5%;
+  font-size: x-large;
+  font-weight: 600
+`;
+
+const DescriptionDiv = styled.div`
+    grid-area: 2 / 2 / 6 / 7;
+    border: 2px solid #213547;
+    padding: 2rem;
+  `;
+const BirdSummary = styled.div`
+
+
+`;
+
+
+
+const PicsAndNotes = styled.div`
 
 `;
 
@@ -61,5 +115,9 @@ export {
   CardHolderDiv,
   CardDiv,
   ImgDiv,
-  HeaderDiv
+  HeaderDiv,
+  BirdDetailDiv,
+  BirdDetailHeader,
+  BirdDetailBackground,
+  DescriptionDiv
 }
